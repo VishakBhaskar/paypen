34997ad0a9b7a3388ced023ab4f7586792c02cd1
Vishak Bhaskar <vishakbhaskar.619@gmail.com>
2023-04-24 00:02:46 +0530

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   darkMode: "media",
   content: [
-    "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
